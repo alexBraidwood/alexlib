e71ad43c9a436ce970666bf7f090b8092e7aab9a
Alex Braidwood <alex.braidwood@thebatforge.com>
2017-12-21 18:47:19 -0500

    Added starting point

---
+++ include/alexlib.h
@@ -0,0 +1,11 @@
+/*
+ * File: alexlib.h.
+ * Created by: Alex Braidwood.
+ * Date: Dec 21, 2017.
+ * Notice: Copyright (c) 2017 The Bat Forge. All Rights Reserved.
+ */
+
+#ifndef ALEXLIB_ALEXLIB_H
+#define ALEXLIB_ALEXLIB_H
+
+#endif //ALEXLIB_ALEXLIB_H
